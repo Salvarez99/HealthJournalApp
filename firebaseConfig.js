0921e0f7b15afd9a39ef51a613c8d8380baba779
SeoyeonChoi23 <seoyeonchio2001@gmail.com>
2024-04-25 23:29:46 -0400

    Added sorting nearest date in journalScreen.js, fixed styling.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,7 @@ import {getAuth} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: "AIzaSyBlE8l_1HGan1DZwo-Lql2rHBFpJwuLwfA", authDomain: "health-journaling-app.firebaseapp.com", projectId: "health-journaling-app", storageBucket: "health-journaling-app.appspot.com", messagingSenderId: "410912398218", appId: "1=410912398218=web=e54a2e1153d6213d75c673", measurementId: "G-HFDYYR7V90" 
 };
 
 
