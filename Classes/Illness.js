88fc5e7691549b447ee56f93449aed9c9ec9f499
Stephen <Stephenalvarez3@gmail.com>
2024-05-06 19:27:16 -0400

    Documented Classes

--- Classes/Illness.js
+++ Classes/Illness.js
@@ -1,18 +1,43 @@
+/***************************************************************************************
+ * Authors: Stephen Alvarez
+ * Date: 5/1/2024
+ * Code Version: 1.0
+ * 
+ * Description:
+ *  A class to wrap all illness data in one place for easy access later on.
+ * 
+ * 
+ ***************************************************************************************/
+
 export default class Illness{
+    /**
+     * @param {String} name 
+     * @param {String} startDate 
+     * @param {String} endDate 
+     */
     constructor(name, startDate, endDate){
         this._name = name;
         this._startDate = startDate;
         this._endDate = endDate;
     }
 
+    /**
+     * @return {String}
+     */
     get name() {
         return this._name;
     }
 
+    /**
+     * @return {String}
+     */
     get startDate() {
         return this._startDate;
     }
 
+    /**
+     * @return {String}
+     */
     get endDate() {
         return this._endDate;
     }
