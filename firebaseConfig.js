bc4e3e8a90d967f82bb3effac30d6f6e18f0117e
Stephen <Stephenalvarez3@gmail.com>
2024-05-06 22:03:51 -0400

    .

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getStorage} from "firebase/storage";
-import {getAuth} from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBlE8l_1HGan1DZwo-Lql2rHBFpJwuLwfA", authDomain: "health-journaling-app.firebaseapp.com", projectId: "health-journaling-app", storageBucket: "health-journaling-app.appspot.com", messagingSenderId: "410912398218", appId: "1=410912398218=web=e54a2e1153d6213d75c673", measurementId: "G-HFDYYR7V90" 
+  apiKey: "AIzaSyBlE8l_1HGan1DZwo-Lql2rHBFpJwuLwfA",
+  authDomain: "health-journaling-app.firebaseapp.com",
+  projectId: "health-journaling-app",
+  storageBucket: "health-journaling-app.appspot.com",
+  messagingSenderId: "410912398218",
+  appId: "1=410912398218=web=e54a2e1153d6213d75c673",
+  measurementId: "G-HFDYYR7V90",
 };
-
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
@@ -21,4 +26,4 @@ export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 
 //export default firebaseConfig;
 //export{storage};
-//Initializing Firebase Authentification 
+//Initializing Firebase Authentification
