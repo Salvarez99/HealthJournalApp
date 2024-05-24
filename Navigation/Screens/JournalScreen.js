93ba542164defe92f6d69c8ebf9c536a72ddef89
Salvarez99 <stephenalvarez3@gmail.com>
2024-05-24 09:09:14 -0400

    .

--- Navigation/Screens/JournalScreen.js
+++ Navigation/Screens/JournalScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useFocusEffect } from "@react-navigation/native";
 import {
   View,
@@ -37,6 +37,10 @@ export default function JournalScreen({ navigation }) {
       fetchJournalData();
     }, [])
   );
+
+  useEffect(() => {
+    fetchJournalData();
+  }, []);
   const openModal1 = () => {
     setSelectedModal("AddAppointmentForm");
     setIsModalVisible(true);
