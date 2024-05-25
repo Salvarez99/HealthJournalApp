8cf228f697b89baed4360e2cfa68312275b73b1f
Salvarez99 <stephenalvarez3@gmail.com>
2024-05-25 00:33:13 -0400

    fixed env file

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,29 +1,36 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
-import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getStorage } from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from '@env';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "Put API Key here",
-  authDomain: "Put domain here", 
-  projectId: "Put project Id Here", 
-  storageBucket: "Put Storage Bucket here", 
-  messagingSenderId: "Put sender ID here", 
-  appId: "Put AppId here", 
-  measurementId: "Put MeasurementId here" 
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-//const analytics = getAnalytics(app);
-//const storage = getStorage(app);
+// const analytics = getAnalytics(app);
+// const storage = getStorage(app);
 
-//export default firebaseConfig;
-//export{storage};
-//Initializing Firebase Authentification
+// export default firebaseConfig;
+// export { storage };
+// Initializing Firebase Authentication
